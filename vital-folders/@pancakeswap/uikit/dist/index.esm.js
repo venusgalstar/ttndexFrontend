3a59dde9d207a155a2d54ae2bb070eacc9c11100
venusgalstar <venusgalstar@gmail.com>
2022-09-17 17:36:10 +0000

    remove social link

--- vital-folders/@pancakeswap/uikit/dist/index.esm.js
+++ vital-folders/@pancakeswap/uikit/dist/index.esm.js
@@ -3625,11 +3625,11 @@ var links = [
     },
 ];
 var socials = [
-    {
-        label: "Github",
-        icon: "GithubIcon",
-        href: "https://github.com/Bridgeswap-Dex",
-    },
+    // {
+    //     label: "Github",
+    //     icon: "GithubIcon",
+    //     href: "https://github.com/Bridgeswap-Dex",
+    // },
     {
         label: "Twitter",
         icon: "TwitterIcon",
@@ -3640,69 +3640,69 @@ var socials = [
         icon: "MediumIcon",
         href: "https://thetokennetwork.medium.com/",
     },
-    {
-        label: "Gitbook",
-        icon: "GitbookIcon",
-        href: "https://bridgeswap-exchange-1.gitbook.io/bridgeswap/",
-    },
-    {
-        label: "Telegram",
-        icon: "TelegramIcon",
-        items: [
-            {
-                label: "English",
-                href: "https://t.me/bridgeswap",
-            },
-            // {
-            //   label: "Bahasa Indonesia",
-            //   href: "https://t.me/PancakeSwapIndonesia",
-            // },
-            // {
-            //   label: "中文",
-            //   href: "https://t.me/PancakeSwap_CN",
-            // },
-            // {
-            //   label: "Tiếng Việt",
-            //   href: "https://t.me/PancakeSwapVN",
-            // },
-            // {
-            //   label: "Italiano",
-            //   href: "https://t.me/pancakeswap_ita",
-            // },
-            // {
-            //   label: "русский",
-            //   href: "https://t.me/pancakeswap_ru",
-            // },
-            // {
-            //   label: "Türkiye",
-            //   href: "https://t.me/pancakeswapturkiye",
-            // },
-            // {
-            //   label: "Português",
-            //   href: "https://t.me/PancakeSwapPortuguese",
-            // },
-            // {
-            //   label: "Español",
-            //   href: "https://t.me/PancakeswapEs",
-            // },
-            // {
-            //   label: "日本語",
-            //   href: "https://t.me/pancakeswapjp",
-            // },
-            // {
-            //   label: "Français",
-            //   href: "https://t.me/pancakeswapfr",
-            // },
-            // {
-            //   label: "Announcements",
-            //   href: "https://t.me/PancakeSwapAnn",
-            // },
-            // {
-            //   label: "Whale Alert",
-            //   href: "https://t.me/PancakeSwapWhales",
-            // },
-        ],
-    },
+    // {
+    //     label: "Gitbook",
+    //     icon: "GitbookIcon",
+    //     href: "https://bridgeswap-exchange-1.gitbook.io/bridgeswap/",
+    // },
+    // {
+    //     label: "Telegram",
+    //     icon: "TelegramIcon",
+    //     items: [
+    //         {
+    //             label: "English",
+    //             href: "https://t.me/bridgeswap",
+    //         },
+    //         // {
+    //         //   label: "Bahasa Indonesia",
+    //         //   href: "https://t.me/PancakeSwapIndonesia",
+    //         // },
+    //         // {
+    //         //   label: "中文",
+    //         //   href: "https://t.me/PancakeSwap_CN",
+    //         // },
+    //         // {
+    //         //   label: "Tiếng Việt",
+    //         //   href: "https://t.me/PancakeSwapVN",
+    //         // },
+    //         // {
+    //         //   label: "Italiano",
+    //         //   href: "https://t.me/pancakeswap_ita",
+    //         // },
+    //         // {
+    //         //   label: "русский",
+    //         //   href: "https://t.me/pancakeswap_ru",
+    //         // },
+    //         // {
+    //         //   label: "Türkiye",
+    //         //   href: "https://t.me/pancakeswapturkiye",
+    //         // },
+    //         // {
+    //         //   label: "Português",
+    //         //   href: "https://t.me/PancakeSwapPortuguese",
+    //         // },
+    //         // {
+    //         //   label: "Español",
+    //         //   href: "https://t.me/PancakeswapEs",
+    //         // },
+    //         // {
+    //         //   label: "日本語",
+    //         //   href: "https://t.me/pancakeswapjp",
+    //         // },
+    //         // {
+    //         //   label: "Français",
+    //         //   href: "https://t.me/pancakeswapfr",
+    //         // },
+    //         // {
+    //         //   label: "Announcements",
+    //         //   href: "https://t.me/PancakeSwapAnn",
+    //         // },
+    //         // {
+    //         //   label: "Whale Alert",
+    //         //   href: "https://t.me/PancakeSwapWhales",
+    //         // },
+    //     ],
+    // },
 ];
 var MENU_HEIGHT = 64;
 var MENU_ENTRY_HEIGHT = 48;
