fc6eddbbd7f0de9d550c3aba5a727fe176d6e866
DESKTOP-V2O0D8E\Hari <topdirector2017@gmail.com>
2022-11-02 10:11:28 +0900

    update bank icon

--- vital-folders/@pancakeswap/uikit/dist/index.esm.js
+++ vital-folders/@pancakeswap/uikit/dist/index.esm.js
@@ -3336,7 +3336,7 @@ var Icon$l = function (props) {
 };
 
 var Icon$BankIcon = function (props) {
-    return (React.createElement(Svg, __assign({ height: "20", viewBox: "0 0 20 20", fill: "none" }, props),
+    return (React.createElement(Svg, __assign({ width: "24", height: "24", viewBox: "0 0 16 16", fill: "none" }, props),
         React.createElement("path", { d: "M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z", fillRule: "evenodd" }),
         React.createElement("path", { d: "M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z" }),
         React.createElement("path", { d: "M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z" }),
